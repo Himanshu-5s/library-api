9831c9f4a0bd2c97c2a1b42404d81a843f0a6144
Himanshu <himanshusharma5205@gmail.com>
2024-06-17 15:04:40 +0530

    created post requests

--- main.go
+++ main.go
@@ -7,25 +7,34 @@ import (
 )
 
 type book struct {
-	Id      string
+	ID      string
 	Title   string
 	Author  string
 	Quanity int
 }
 
 var books = []book{
-	{Id: "1", Title: "The Alchemist", Author: "Paulo Coelho", Quanity: 10},
-	{Id: "2", Title: "The Monk Who Sold His Ferrari", Author: "Robin Sharma", Quanity: 5},
-	{Id: "3", Title: "The Power of Now", Author: "Eckhart Tolle", Quanity: 7},
+	{ID: "1", Title: "The Alchemist", Author: "Paulo Coelho", Quanity: 10},
+	{ID: "2", Title: "The Monk Who Sold His Ferrari", Author: "Robin Sharma", Quanity: 5},
+	{ID: "3", Title: "The Power of Now", Author: "Eckhart Tolle", Quanity: 7},
 }
 
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
+}
 
+func createBooks(c *gin.Context) {
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBooks)
 	router.Run("localhost:8080")
 }
